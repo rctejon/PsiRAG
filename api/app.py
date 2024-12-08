e8dde40ae53af7129b9d2126c80384ca2043b43a
rctejon <30982129+rctejon@users.noreply.github.com>
2024-12-08 00:27:16 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -47,83 +47,90 @@ def scrapper():
     driver.get(paper_url)
 
     # Find the main document section using XPath and extract its HTML content
-    document = driver.find_element("xpath", "//article")
-    document = BeautifulSoup(document.get_attribute("innerHTML"), "html.parser")
+    try:
+        document = driver.find_element("xpath", "//article")
+        document = BeautifulSoup(document.get_attribute("innerHTML"), "html.parser")
 
-    # Extract the title of the paper
-    paper_title = document.find("h1").text
+        # Extract the title of the paper
+        paper_title = document.find("h1").text
 
-    # Find the abstract section and extract its title, content, and keywords
-    abstract = document.find("section", {"class": "abstract"})
-    abstract_title = abstract.find("h2").text
-    abstract_content = abstract.find("p").text
-    keywords = abstract.find("section", {"id": "kwd-group1"})
-    keywords_title = keywords.find("strong").text if keywords else ""
-    keywords = keywords.find("p").text.replace(keywords_title, '') if keywords else ""
+        # Find the abstract section and extract its title, content, and keywords
+        abstract = document.find("section", {"class": "abstract"})
+        abstract_title = abstract.find("h2").text
+        abstract_content = abstract.find("p").text
+        keywords = abstract.find("section", {"id": "kwd-group1"})
+        keywords_title = keywords.find("strong").text if keywords else ""
+        keywords = keywords.find("p").text.replace(keywords_title, '') if keywords else ""
 
-    # Find all sections whose IDs start with "Sec" followed by numbers using regex
-    sections = document.find_all('section', id=re.compile(r"^Sec\d+$"))
+        # Find all sections whose IDs start with "Sec" followed by numbers using regex
+        sections = document.find_all('section', id=re.compile(r"^Sec\d+$"))
 
-    if not sections:
-        sections = document.find_all('section', id=re.compile(r"^sec\d+$"))
+        if not sections:
+            sections = document.find_all('section', id=re.compile(r"^sec\d+$"))
 
-    # Initialize a list to hold the text of all sections
-    all_sections = []
+        # Initialize a list to hold the text of all sections
+        all_sections = []
 
-    # Loop through each section found
-    for section in sections:
-        section_texts = section.find_all(["h2", "h3", "h4", "h5", "p", "table"])
-        current_section = ""
-        for section_text in section_texts:
-            prev = ""
-            if section_text.name == "h2":
-                prev = "## "
-            elif section_text.name == "h3":
-                prev = "### "
-            elif section_text.name == "h4":
-                prev = "#### "
-            elif section_text.name == "h5":
-                prev = "##### "
-            elif section_text.name == "table":
-                # Extract the table content and convert to Markdown format
-                table_content = ""
-                divider = ""
-                for row in section_text.find_all("tr"):
-                    cells = row.find_all(["th"])
-                    for cell in cells:
-                        table_content += "| **" + cell.text + "** "
-                        divider += "|:---:"
-                    table_content += "|\n" if divider else "\n"
-                    table_content += divider
+        # Loop through each section found
+        for section in sections:
+            section_texts = section.find_all(["h2", "h3", "h4", "h5", "p", "table"])
+            current_section = ""
+            for section_text in section_texts:
+                prev = ""
+                if section_text.name == "h2":
+                    prev = "## "
+                elif section_text.name == "h3":
+                    prev = "### "
+                elif section_text.name == "h4":
+                    prev = "#### "
+                elif section_text.name == "h5":
+                    prev = "##### "
+                elif section_text.name == "table":
+                    # Extract the table content and convert to Markdown format
+                    table_content = ""
                     divider = ""
-                    cells = row.find_all(["td"])
-                    for cell in cells:
-                        table_content += "| " + cell.text + " "
-                    table_content += "|"
-                table_content += "\n\n"
-                current_section += table_content
-            current_section += prev + section_text.text + "\n"
-        all_sections.append(current_section)
+                    for row in section_text.find_all("tr"):
+                        cells = row.find_all(["th"])
+                        for cell in cells:
+                            table_content += "| **" + cell.text + "** "
+                            divider += "|:---:"
+                        table_content += "|\n" if divider else "\n"
+                        table_content += divider
+                        divider = ""
+                        cells = row.find_all(["td"])
+                        for cell in cells:
+                            table_content += "| " + cell.text + " "
+                        table_content += "|"
+                    table_content += "\n\n"
+                    current_section += table_content
+                current_section += prev + section_text.text + "\n"
+            all_sections.append(current_section)
 
-    # Write the extracted content to a text file
-    file_content = f"# {paper_title}\n\n## {abstract_title}\n{abstract_content}\n\n"
-    file_content += f"**{keywords_title.strip()}**\n{keywords}\n"
-    for section in all_sections:
-        file_content += section + "\n"
+        # Write the extracted content to a text file
+        file_content = f"# {paper_title}\n\n## {abstract_title}\n{abstract_content}\n\n"
+        file_content += f"**{keywords_title.strip()}**\n{keywords}\n"
+        for section in all_sections:
+            file_content += section + "\n"
 
-    # Save to S3
-    s3 = boto3.client('s3', region_name=AWS_REGION)
-    s3_file_key = f"papers/{paper_title}.txt"
+        # Save to S3
+        s3 = boto3.client('s3', region_name=AWS_REGION)
+        s3_file_key = f"papers/{paper_title}.txt"
 
-    s3.put_object(
-        Bucket=S3_BUCKET,
-        Key=s3_file_key,
-        Body=file_content,
-        ContentType='text/plain'
-    )
+        s3.put_object(
+            Bucket=S3_BUCKET,
+            Key=s3_file_key,
+            Body=file_content,
+            ContentType='text/plain'
+        )
+    except Exception as e:
+        paper_title = driver.find_element(By.TAG_NAME, "h1").get_attribute("innerHTML")
+        print(paper_title)
+        s3_file_key = f"papers/{paper_title}.txt"
+
 
     # Upload document to DB using API
     url = "https://psirag.livelydune-e2aafbd7.eastus.azurecontainerapps.io/add-document"
+    print(s3_file_key)
     response = requests.post(
         url,
         data={
